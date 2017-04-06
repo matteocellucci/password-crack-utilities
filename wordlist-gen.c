4fa670a933fa09a9d2074ee6bb2dfa0e63811a96
Matteo Cellucci <matteocellucci@gmail.com>
2017-04-06 17:38:48 +0200

    Optimize wordlist-gen

--- wordlist-gen.c
+++ wordlist-gen.c
@@ -2,36 +2,20 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
-#include <math.h>
 #include "wordlist-gen.h"
 
-#define PRINTABLE_SIZE 96
-
-const static char printable[PRINTABLE_SIZE] = {
-	// alphanumeric
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-	'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-	'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-	'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
-	'8', '9',
-	// punctation
-	'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
-	':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|',
-	'}', '~',
-	// space
-	'\t', ' '
-};
+// TODO togliere il limite ad n
 
 int main(int argc, char *argv[]) {
 	int opt;
 	int hflag = 0;
-	int numvalue = 0;
+	int nvalue = 0;
 
 	opterr = 0;
-	while((opt = getopt(argc, argv, "nh")) != -1) {
+	while((opt = getopt(argc, argv, "n:h")) != -1) {
 		switch(opt) {
 			case 'n':
-				numvalue = atoi(optarg);
+				nvalue = atoi(optarg);
 				break;
 			case 'h':
 				hflag = 1;
@@ -54,23 +38,47 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_SUCCESS);
 	}
 
-	if(optind == 1 || numvalue == 0) {
-		fprintf(stderr, "Error: use -n to specify depth\a\n");
+	if(optind == 1 || nvalue == 0) {
+		fprintf(stderr, "Error: use -nNUM to specify depth\a\n");
 		exit(EXIT_FAILURE);
 	}
 
-	FILE *list = fopen("wordlist-gen.txt","w");
-	combine(list, numvalue);
+	FILE *list = fopen("wordlist-gen.txt", "w");
+	combine(list, nvalue);
 	fclose(list);
 
 	return 0;
 }
 
 void combine(FILE *list, int depth) {
-	for(unsigned long long int i = 0; i < pow(PRINTABLE_SIZE, depth); i++) {
-		fputc(printable[i % PRINTABLE_SIZE], list);
-		if(depth > 1) {
-			combine(list, depth - 1);
+	for(int a = 0; a < PRINTABLE_SIZE && depth > 0; a++) {
+		fputc(printable[a], list);
+		fputc(10, list);
+		for(int b = 0; b < PRINTABLE_SIZE && depth > 1; b++) {
+			fputc(printable[a], list);
+			fputc(printable[b], list);
+			fputc(10, list);
+			for(int c = 0; c < PRINTABLE_SIZE && depth > 2; c++) {
+				fputc(printable[a], list);
+				fputc(printable[b], list);
+				fputc(printable[c], list);
+				fputc(10, list);
+				for(int d = 0; d < PRINTABLE_SIZE && depth > 3; d++) {
+					fputc(printable[a], list);
+					fputc(printable[b], list);
+					fputc(printable[c], list);
+					fputc(printable[d], list);
+					fputc(10, list);
+					for(int e = 0; e < PRINTABLE_SIZE && depth > 4; e++) {
+						fputc(printable[a], list);
+						fputc(printable[b], list);
+						fputc(printable[c], list);
+						fputc(printable[d], list);
+						fputc(printable[e], list);
+						fputc(10, list);
+					}
+				}
+			}
 		}
 	}
 }
@@ -79,6 +87,6 @@ void usage() {
 	printf("Worldlist generator\n");
 	printf("<link to github>\n\n");
 	printf("Usage: wlgen [OPTION]\n");
-	printf("-nNUM\tRecursion depth, it generates combinations of NUM characters\n");
+	printf("-nNUM\tRecursion depth, it generates combinations of NUM characters (max 5)\n");
 	printf("-h\tPrint this message\n");
 }
--- wordlist-gen.h
+++ wordlist-gen.h
@@ -1,3 +1,6 @@
+
+#define PRINTABLE_SIZE 96
+
 /*
  * Stampa il manuale d'uso
  */
@@ -8,3 +11,21 @@ void usage();
  * in input
  */
 void combine(FILE *, int);
+
+/*
+ * CHARSER
+ */
+const static char printable[] = {
+	// alphanumeric
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+	'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+	'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+	'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9',
+	// punctation
+	'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+	':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|',
+	'}', '~',
+	// space
+	'\t', ' '
+};
